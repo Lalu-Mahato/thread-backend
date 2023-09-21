711d20431d8389ca360edc38a7f60fa3125f2f1c
Lalu-Mahato <135944020+Lalu-Mahato@users.noreply.github.com>
2023-09-21 12:05:40 +0530

    prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "./lib/db";
 
 async function init() {
     const app = express();
@@ -13,11 +14,31 @@ async function init() {
                 hello: String
                 say(name: String): String
             }
+            type Mutation {
+                createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+
+            }
         `,
         resolvers: {
             Query: {
                 hello: () => "Hello Graphql Server",
                 say: (_, { name }: { name: string }) => `Hey ${name}, How are you?`
+            },
+            Mutation: {
+                createUser: async (_, { firstName, lastName, email, password }:
+                    { firstName: string; lastName: string; email: string; password: string; }) => {
+                    await prismaClient.user.create({
+                        data: {
+                            firstName,
+                            lastName,
+                            email,
+                            password,
+                            // profileImageUrl: "",
+                            salt: "random_salt",
+                        }
+                    })
+                    return true;
+                },
             }
         },
     });
